620eb451deafd57f3ae922d8b8287d01b5308ea4
oceanasrat <wykiyanosa@gmail.com>
2026-02-28 19:18:35 -0600

    Refactor main.jsx to remove AuthWrapper
    Removed AuthWrapper component and updated App import.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import "./index.css";
-import AuthWrapper from "./auth/AuthWrapper.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthWrapper>
-      <App />
-    </AuthWrapper>
+    <App />
   </React.StrictMode>
 );
